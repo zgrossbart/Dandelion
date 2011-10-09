8a2b02a1bdb3d83a7e9e1eb9f911b1dd42bc3e17
Zack Grossbart <zack@grossbart.com>
2011-10-09 09:36:06 -0400

    We are now showing error messages properly in the code manager.

--- codeMgr.js
+++ codeMgr.js
@@ -1,14 +1,62 @@
+/******************************************************************************* 
+ * 
+ * Copyright 2011 Zack Grossbart 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+ 
+/**
+ * The code manager handles editing the animation source code in 
+ * the page.  It isn't required to make the animation run.
+ */
 codeMgr = {
     runScript: function() {
-        // Update script to edited version
-    	var code = codeMgr.editor.getValue();
-        var canvas = codeMgr.scope.view.canvas;
-        codeMgr.scope.clear();
-        codeMgr.scope.setup(canvas);
-    	codeMgr.scope.evaluate(code);
-        
-        $('#editContainer').hide();
-        $('#container').show();
+        try {
+            // Update script to edited version
+            var code = codeMgr.editor.getValue();
+            var canvas = codeMgr.scope.view.canvas;
+            codeMgr.scope.clear();
+            codeMgr.scope.setup(canvas);
+            codeMgr.scope.evaluate(code);
+            
+            codeMgr.hideError();
+            $('#editContainer').hide();
+            $('#container').show();
+        } catch (e) {
+            codeMgr.showError(e);
+        }
+    },
+    
+    hideError: function(/*string*/ err) {
+        if ($('#err').is(":visible")) {
+            $('#err').hide();
+            $('#editContainer').css('height', ($(window).height() - 75) + 'px');
+            $('div.fullscreen').css('top', '0px');
+             
+            codeMgr.editor.focus();
+            codeMgr.editor.refresh();
+        }
+    },
+    
+    showError: function(/*string*/ err) {
+        console.log('err: ' + err);
+        $('#err').show().text('Error running script: ' + err);
+        $('#editContainer').css('height', ($(window).height() - 175) + 'px');
+        $('div.fullscreen').css('top', '100px');
+         
+        codeMgr.editor.focus();
+        codeMgr.editor.refresh();
     },
     
     editScript: function() {
